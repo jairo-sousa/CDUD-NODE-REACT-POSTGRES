e8dd719c6ffc254a2f97ea84c8f3c54e58e9339c
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-05-10 15:01:51 -0300

    feat: configure server to listen on port 8800

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from "express";
+import cors from "cors";
+
+const PORT = 8800;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.send("HOME");
+});
+
+app.listen(PORT);
+console.log(`listening on port: ${PORT}`);
