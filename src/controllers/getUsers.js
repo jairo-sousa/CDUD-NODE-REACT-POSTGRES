ff681bf70d777eee7da7d870a329744b7342dd35
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-05-11 14:44:10 -0300

    feat: create user controller

---
+++ src/controllers/getUsers.js
@@ -0,0 +1,13 @@
+import { db } from "../db.js";
+
+export const getUsers = (_, res) => {
+	const query = "SELECT * FROM users";
+
+	db.connect();
+
+	db.query(query, (err, data) => {
+		if (err) return res.json(err);
+
+		return res.status(200).json(data);
+	});
+};
