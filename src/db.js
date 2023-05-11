55182f235d4e9df5409d566b1881d5cbdff90a2d
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-05-11 14:40:05 -0300

    hotfix: use pg instead of postgres

--- src/db.js
+++ src/db.js
@@ -1,12 +1,14 @@
-import postgres from "postgres";
+import pg from "pg";
+const { Client } = pg;
 import * as dotenv from "dotenv";
 
 dotenv.config();
 const USER = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 
-export const db = postgres({
+export const db = new Client({
 	host: "localhost",
+	port: 5432,
 	user: USER,
 	password: PASSWORD,
 	database: "USER_CRUD",
