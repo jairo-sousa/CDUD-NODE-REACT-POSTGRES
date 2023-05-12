e0353f43764aa4086cfd0f3bfe8325bfc5925489
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-05-12 18:28:58 -0300

    style: set App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import { Flex, Heading } from "@chakra-ui/react";
 import "./styles/global.css";
+import { toast, ToastContainer } from "react-toastify";
+import { Form } from "./components/Form";
+
 export function App() {
-	return <h1>TODO APP</h1>;
+	return (
+		<Flex
+			w="100%"
+			maxW="80rem"
+			mt="2rem"
+			direction="column"
+			align="center"
+			gap="1rem"
+		>
+			<Heading as="h1" fontSize="3.2rem">
+				USERS
+			</Heading>
+			<Form />
+			<ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+		</Flex>
+	);
 }
