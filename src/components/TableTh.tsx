794d19621e8b035e8137d6c8e5c83be22d623e08
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-05-12 19:23:16 -0300

    style(TableTh): set Props

--- src/components/TableTh.tsx
+++ src/components/TableTh.tsx
@@ -1,10 +1,23 @@
 import { Th } from "@chakra-ui/react";
+import React from "react";
 import { useMediaQuery } from "@chakra-ui/react";
 
-export function TableTh() {
+type TableThProps = {
+	label?: string;
+	children?: React.ReactNode;
+	onlyWeb?: boolean;
+};
+export function TableTh({ label, children, onlyWeb }: TableThProps) {
 	const [isSmallerThan500] = useMediaQuery("(min-width: 50rem)");
 
 	return (
-		<Th textAlign="start" borderBottom="inset" pb="0.5rem" display={``}></Th>
+		<Th
+			textAlign="start"
+			borderBottom="inset"
+			pb="0.5rem"
+			display={`${isSmallerThan500 && onlyWeb && "none"}`}
+		>
+			{label || children}
+		</Th>
 	);
 }
