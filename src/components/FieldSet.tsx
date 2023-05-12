b752c15b700733e372a924efa2ca08452cd94c1c
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-05-12 13:32:30 -0300

    style: set FieldSet

--- src/components/FieldSet.tsx
+++ src/components/FieldSet.tsx
@@ -1,3 +1,18 @@
+import { Flex, Input, Text } from "@chakra-ui/react";
+
 export function Fieldset() {
-	return <p>Fieldset</p>;
+	return (
+		<Flex as="fieldset" direction="column">
+			<Text as="label">Name</Text>
+			<Input
+				w="12rem"
+				h="4rem"
+				px="1rem"
+				border="0.1rem solid #bbb"
+				borderRadius="0.5rem"
+				name={"name"}
+				type={"text"}
+			></Input>
+		</Flex>
+	);
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import { Flex } from "@chakra-ui/react";
 import { useRef } from "react";
+import { Fieldset } from "./FieldSet";
 
 export function Form({ onEdit }: any) {
 	const formRef = useRef<HTMLFormElement>(null);
@@ -16,7 +17,7 @@ export function Form({ onEdit }: any) {
 			borderRadius="0.5rem"
 			boxShadow="0 0 0.5rem #ccc"
 		>
-			<Flex as="fieldset" direction="column"></Flex>
+			<Fieldset />
 		</Flex>
 	);
 }
