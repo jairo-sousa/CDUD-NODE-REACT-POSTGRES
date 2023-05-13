bc75ba93dcf4840ebce67c9ce1bf0222ab43b9cc
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-05-13 10:31:29 -0300

    feat: use getUsers

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,28 @@ import { Flex, Heading } from "@chakra-ui/react";
 import "./styles/global.css";
 import { toast, ToastContainer } from "react-toastify";
 import { Form } from "./components/Form";
+import { DataTable } from "./components/DataTable";
+import { useEffect, useState } from "react";
+import { User } from "./types/User";
+import axios from "axios";
 
 export function App() {
+	const [users, setUsers] = useState<User[]>();
+	const [onEdit, setOnEdit] = useState(null);
+
+	const getUsers = async () => {
+		try {
+			const res = await axios.get("http://localhost:8800");
+			setUsers(res.data.sort((a: User, b: User) => (a.name > b.name ? 1 : -1)));
+		} catch (error: any) {
+			toast.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getUsers();
+	}, [setUsers]);
+
 	return (
 		<Flex
 			w="100%"
@@ -17,6 +37,7 @@ export function App() {
 				USERS
 			</Heading>
 			<Form />
+			<DataTable users={users} />
 			<ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
 		</Flex>
 	);
