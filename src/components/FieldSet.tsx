aa14a6b83de728178ccf2d96102ee141e4b76014
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-05-14 20:42:45 -0300

    feat: set innerRef

--- src/components/FieldSet.tsx
+++ src/components/FieldSet.tsx
@@ -1,9 +1,10 @@
 import { Flex, Input, Text } from "@chakra-ui/react";
 type FieldSetProps = {
+	innerRef: React.Ref<HTMLInputElement>;
 	label: string;
 	type?: string;
 };
-export function Fieldset({ label, type }: FieldSetProps) {
+export function Fieldset({ label, type, innerRef }: FieldSetProps) {
 	return (
 		<Flex as="fieldset" direction="column">
 			<Text as="label">{label}</Text>
@@ -13,6 +14,7 @@ export function Fieldset({ label, type }: FieldSetProps) {
 				px="1rem"
 				border="0.1rem solid #bbb"
 				borderRadius="0.5rem"
+				ref={innerRef}
 				name={label}
 				type={type && type}
 			></Input>
